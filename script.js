aa547f907b134abedc78667a50ae2303b084f7e8
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2020-12-01 13:17:03 +0200

    more classes added to HTML, calculator works but not perfect

--- script.js
+++ script.js
@@ -1,12 +1,174 @@
-const container = document.querySelector(".content-container");
-const calculator = container.querySelector(".calculator")
-const keys = document.querySelectorAll('.key');
+const calculator = document.querySelector(".calculator");
+const numButtons = document.querySelectorAll(".btn-num");
+const operatorButtons = document.querySelectorAll(".btn-operator");
+const decimal = document.getElementById("decimal");
+const clearAllButton = document.getElementById("clear-all");
 const screen = document.querySelector(".screen");
 
-const previousKeyType = calculator.dataset.previousKeyType;
+let currentNum = 0;
+let pendingNum;
+let evalStringArr = [];
 
-console.log(keys)
+let operator;
 
+
+function showNumbers(event) {
+    let btnText = event.target.innerText;
+
+    if(currentNum == "0") {
+        currentNum = "" ;
+    }
+
+    currentNum = currentNum + btnText;
+    screen.innerText = currentNum;
+
+    console.log(btnText);
+}
+
+function changeNumDisplay() {
+    pendingNum = currentNum;
+    currentNum = 0;
+    evalStringArr.push(pendingNum);
+    console.log(evalStringArr);
+}
+
+function calculate(event) {
+    operator = event.target.innerText;
+
+    switch (operator) {
+        case '+':
+            changeNumDisplay();
+            evalStringArr.push('+');
+            break;
+        case '-' :
+            changeNumDisplay();
+            evalStringArr.push('-');
+            break;
+        case '%':
+            changeNumDisplay();
+            evalStringArr.push('/');
+            break;
+        case '*':
+            changeNumDisplay()
+            evalStringArr.push('*');
+            break;
+        case '=':
+            changeNumDisplay()
+            let evaluate = eval(evalStringArr.join(' '));
+            currentNum = evaluate + '';
+            screen.innerText = currentNum;
+            evalStringArr = [];
+            console.log(evaluate);
+            break;                
+    }
+}
+
+
+function addDecimal() {
+    if(!currentNum.includes('.')) {
+        currentNum = currentNum + '.';
+    }
+    screen.innerText = currentNum;
+}
+
+
+clearAllButton.addEventListener('click', function() {
+    currentNum = 0;
+    pendingNum = 0;
+    evalStringArr = [];
+    operator = null;
+    screen.innerText = currentNum;
+    console.log('clear all clicked');
+})
+
+numButtons.forEach(function(button) {
+    button.addEventListener("click", function(e) {
+        showNumbers(e);
+        addDecimal()
+    })
+})
+
+operatorButtons.forEach(function(button) {
+    button.addEventListener("click", function(e) {
+        calculate(e) 
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function add(a, b) {
     return a + b;
 }
@@ -27,20 +189,22 @@ function operate(func, a, b) {
     return func(a,b)
 }
 
+ 
 
 function showNumbers(e) {
     if(e.target.matches('.key')) {
-        const key = e.target;
-        const action = key.dataset.action;
-        const keyContent = key.textContent;
-        const displayedNum = screen.textContent;
-
+        const button = e.target; //This is the whole div, what i pressed
+        const action = button.dataset.action; //This shows the data-action that i pressed        
+        const buttonContent = button.textContent; // shows what is inside the div that i pressed. In this case what is inside the kbd element
+        const displayedNum = screen.textContent; //What you see in the calculators screen
+        let operator;
+//console.log(numButton)
         if (!action) {
 
-            if (displayedNum === '0' || previousKeyType === 'operator') {
-                screen.textContent = keyContent
+            if (displayedNum === '0') {
+                screen.textContent = buttonContent;
             } else {
-                screen.textContent = displayedNum + keyContent
+                screen.textContent = displayedNum + buttonContent
             }
         }
 
@@ -49,14 +213,16 @@ function showNumbers(e) {
         }
 
         if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-                screen.textContent = displayedNum + keyContent;
-                calculator.dataset.previousKeyType = 'operator';
-                console.log("operator key");
+                screen.textContent = displayedNum + buttonContent;
+                operator = buttonContent;
+
+                console.log(operator);
+                console.log("operator button");
         }
         
         if (action === 'clear-all') {
             screen.textContent = '';
-            console.log('clear all key!')
+            console.log('clear all button!')
         }
 
         if (action === 'clear') {
@@ -68,7 +234,7 @@ function showNumbers(e) {
             let firstValue = calculator.dataset.firstValue;
             let operator = calculator.dataset.action;
             let secondValue = displayedNum;
-            console.log('equal key!')
+            
         }
     }
 }
@@ -77,9 +243,5 @@ function showNumbers(e) {
 
 calculator.addEventListener('click', showNumbers)
 
-keys.forEach(function(key) {
-    key.addEventListener('click', calculate);
-    console.log(key);
-})
-
+*/
 
